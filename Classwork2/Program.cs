5bb3da6ae13fea33b24a53a8b44f9cd4392e7f16
Zach Coomer <zaccoom@ut.utm.edu>
2023-01-19 19:47:58 -0600

    Hello World

--- Classwork2/Program.cs
+++ Classwork2/Program.cs
@@ -11,24 +11,11 @@ namespace Classwork2
 
 
     class Program
-    {
-        private int steve;
-
-        public void setSteve(int s)
-        {
-            steve = s;
-        }
-        public int getSteve()
-        {
-            return steve;
-        }
+    {  
+    
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world" + 5);
-            Console.WriteLine("setting steve to 4");
-            Program p = new Program();
-            p.setSteve(4);
-            Console.WriteLine("Steve is: " + p.getSteve());
+            Console.WriteLine("Hello World!");
             Console.ReadKey();
 
 
