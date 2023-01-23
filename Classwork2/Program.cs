8a7fe1d68274107d59fa51626e96503935c36818
Zach Coomer <zaccoom@ut.utm.edu>
2023-01-22 22:01:45 -0600

    Number Guessing Game implemented!

--- Classwork2/Program.cs
+++ Classwork2/Program.cs
@@ -15,7 +15,36 @@ namespace Classwork2
     
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random ranNum = new Random();
+            int userGuess = -1;
+            int guesses = 0;
+
+            Console.WriteLine("I am thinking of a number between 0 and 100.");
+
+            int num = ranNum.Next(0, 101);
+
+            while(userGuess != num)
+            {
+                Console.WriteLine("Try a guess.");
+                userGuess = Convert.ToInt32(Console.ReadLine());
+                guesses++;
+
+                if(userGuess > num)
+                {
+                    Console.WriteLine("Your guess is too high.");
+                }
+                else if (userGuess < num)
+                {
+                    Console.WriteLine("Your guess is too low.");
+                }
+                else
+                {
+                    Console.WriteLine("That is correct!");
+                }
+            }
+
+            Console.WriteLine("It took you " + guesses + " guesses to guess the number.");
+            
             Console.ReadKey();
 
 
